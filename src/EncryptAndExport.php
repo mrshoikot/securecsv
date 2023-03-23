31f8ce571c1c2eb02e710fd37fba630792d16cff
mrshoikot <mrshoikot@gmail.com>
2023-03-23 19:47:53 +0600

    Implement all functionalities in package primary class
    Now, the features can be used from both CLI and programming interface.

--- src/EncryptAndExport.php
+++ src/EncryptAndExport.php
@@ -1,7 +1,159 @@
 <?php
 
 namespace Mrshoikot\EncryptAndExport;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\DB;
 
 class EncryptAndExport
 {
+    /**
+     * The name of the table to export
+     * 
+     * @var string
+     */
+    private $table;
+
+    /**
+     * The columns that are going to be encrypted
+     * 
+     * @var array
+     */
+    private $selectedColumns;
+
+    /**
+     * The path where the CSV file will be exported
+     * 
+     * @var string
+     */
+    private $path;
+
+
+    /**
+     * All the columns in the table
+     * 
+     * @var array
+     */
+    public $allColumns;
+
+    /**
+     * Create a new instance of EncryptAndExport
+     * 
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->selectedColumns = [];
+
+        // Set the default path to /storage/app
+        $this->path = storage_path('app');
+    }
+
+
+    /**
+     * Set the table name
+     * 
+     * @param string $table
+     * @return void
+     */
+    public function setTable($table){
+
+        // Check if the table exists in the database
+        if (! Schema::hasTable($table)) {
+            throw new \InvalidArgumentException("The table {$table} does not exist in the database.");
+        }
+
+        $this->table = $table;
+        $this->allColumns = Schema::getColumnListing($this->table);
+    }
+
+    /**
+     * Select a column to encrypt
+     * 
+     * @param string $column
+     * @return void
+     */
+    public function selectColumn($column)
+    {
+        // Check if the column exists in the table
+        if (! in_array($column, $this->allColumns)) {
+            throw new \InvalidArgumentException(trans('encrypt-and-export::translations.invalid_column'));
+        }
+
+        $this->selectedColumns[] = $column;
+    }
+
+    /**
+     * Set the path where the CSV file will be exported
+     * 
+     * @param string $path
+     * @return void
+     */
+    public function setPath($path)
+    {
+        // Check if the path is a directory
+        if (! File::isDirectory($path)) {
+            throw new \InvalidArgumentException(trans('encrypt-and-export::translations.invalid_path'));
+        }
+
+        // Check if the path is writable
+        if (! File::isWritable($path)) {
+            throw new \InvalidArgumentException(trans('encrypt-and-export::translations.not_writable', ['path' => $path]));
+        }
+
+        $this->path = $path;
+    }
+
+    /**
+     * Export the data to a CSV file
+     * 
+     * @return void
+     */
+    public function export(): void
+    {
+        $file_name = $this->table.'_'.date('Y-m-d_H-i-s').'.csv';
+        $this->path = $this->path.'/'.$file_name;
+
+        $query = DB::table($this->table);
+
+        $query->orderBy('created_at')->chunk(100, function ($rows) {
+            $rows = $rows->toArray();
+
+            $data = [];
+
+            foreach ($rows as $row) {
+                $row = (array) $row;
+
+                // Encrypt the selected columns
+                foreach ($this->selectedColumns as $column) {
+                    $row[$column] = encrypt($row[$column]);
+                }
+
+                $data[] = $row;
+            }
+
+            $this->writeToCsv($data, $this->path);
+        });
+
+    }
+
+    /**
+     * Write the data to a CSV file
+     * 
+     * @param array $data
+     * @param string $file_path
+     * @return void
+     */
+    protected function writeToCsv(array $data, string $file_path): void
+    {
+        $file = fopen($file_path, 'a');
+
+        foreach ($data as $row) {
+            fputcsv($file, $row);
+        }
+
+        fclose($file);
+    }
+
+    
 }
